90db301baf8940d84b1de99cc48c3ce957ea8ae9
ToolMan47 <kh88403@gmail.com>
2023-05-07 13:57:58 +0800

    init commit

---
+++ src/test/java/com/toolman/pipe/PipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolman.pipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
